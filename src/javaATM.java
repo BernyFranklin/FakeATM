3d9950d1ebd44a9a2077aee4057de79695038b5e
BernyFranklin <98352952+BernyFranklin@users.noreply.github.com>
2022-05-12 12:34:57 -0700

    Added header, imported classes.

---
+++ src/javaATM.java
@@ -0,0 +1,18 @@
+/*
+ * fakeATM Version#1.0
+ * Frank Bernal
+ * CIS 084 Java Programming
+ * Input
+ * Output
+ * 12 May 2022
+ */
+
+ import java.util.Scanner;
+ import java.io.RandomAccessFile;
+ import java.io.FileNotFoundException;
+ import java.io.EOFException;
+ 
+
+public class javaATM {
+    
+}   // End of javaATM
